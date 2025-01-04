20af9a6a5f35ec4aa7f7be5a92a98a56cc37ae2b
KangHoML <kanghokhkh1@gmail.com>
2025-01-04 17:40:32 +0900

    programmers/ch7/p28. 가장 큰 수
    - cmp_to_key를 활용하여 두 가지 기준을 바탕으로 비교

--- programmers/ch7/p28.py
+++ programmers/ch7/p28.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 def sol(numbers):
     numbers = [str(n) for n in numbers]
     numbers.sort(key=lambda x: x * 3, reverse=True)
@@ -7,6 +9,11 @@ def sol(numbers):
     if '0' * len(numbers) == ans: return '0'
     
     return ans
+
+def sol2(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key= cmp_to_key(lambda x, y: int(x + y) - int(y + x)), reverse=True)
+    return str(int(''.join(numbers)))
 
 if __name__ == '__main__':
     numbers = [3, 30, 34, 5, 9]
