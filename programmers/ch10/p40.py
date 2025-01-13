1e1412c65f4500b677a40d5bed97a0e57ba54e9c
KangHoML <kanghokhkh1@gmail.com>
2025-01-13 20:38:48 +0900

    programmers/ch10/p40. 정수 삼각형
    - update sol3. bottom-up 방식으로 작성

--- programmers/ch10/p40.py
+++ programmers/ch10/p40.py
@@ -31,5 +31,13 @@ def sol2(triangle):
     
     return max(dp[-1])
 
+def sol3(triangle):
+    height = len(triangle) - 1
+
+    while height > 0:
+        for i in range(height):
+            triangle[height-1][i] += max(triangle[height][i], triangle[height][i+1])
+
+    return triangle[0][0]
 
     
